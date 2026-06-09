a051cacc86ddb40b03ae67e031d4e7c52b7f9e52
Zi Yin <ziyin@zi-macmini-codex.tailc73f40.ts.net>
2026-06-09 21:41:28 +0800

    chore: update QHRB net worth data for 2026-06-09

--- zapp/sw.js
+++ zapp/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "zapp-store-v37-20260608-qhrb-monitor";
+const CACHE_VERSION = "zapp-store-v37-20260609-qhrb-monitor";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const STATIC_ASSETS = [
   "./",
