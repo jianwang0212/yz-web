dc7063b98d36a898ed153798ff222de8ff7b8315
Zi Yin <ziyin@zi-macmini-codex.tailc73f40.ts.net>
2026-06-12 21:39:16 +0800

    qhrb net worth update 2026-06-12

--- zapp/sw.js
+++ zapp/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "zapp-store-v38-20260612-gt";
+const CACHE_VERSION = "zapp-store-v37-20260612-qhrb-monitor";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const STATIC_ASSETS = [
   "./",
