931f93daa558ba25d8eb4b095a55d29db70ec23d
Zi Yin <ziyin@zi-macmini-codex.tailc73f40.ts.net>
2026-06-13 15:47:36 +0800

    Update Zapp GT data through 2026-06-13

--- zapp/sw.js
+++ zapp/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "zapp-store-v37-20260612-qhrb-monitor";
+const CACHE_VERSION = "zapp-store-v38-20260613-gt";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const STATIC_ASSETS = [
   "./",
