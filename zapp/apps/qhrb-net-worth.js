db5a5762ee08a775167a3f8adb5d5d39d82a16e7
ziyin <ziyin@example.com>
2026-06-24 21:41:44 +0800

    Update qhrb net worth snapshot 2026-06-24

--- zapp/apps/qhrb-net-worth.js
+++ zapp/apps/qhrb-net-worth.js
@@ -1,4 +1,4 @@
-const DATA_URL = "qhrb-san-ci-can-sai-data.json?v=20260623";
+const DATA_URL = "qhrb-san-ci-can-sai-data.json?v=20260624";
 const UPDATE_LOG_URL = "qhrb-net-worth-update-log.json";
 
 const els = {
