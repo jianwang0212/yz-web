676729e91f034558bf6490b800c485ebed856b05
ziyin <ziyin@example.com>
2026-07-23 21:40:01 +0800

    Update QHRB net worth data for 2026-07-23

--- zapp/sw.js
+++ zapp/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "zapp-store-v37-20260722-qhrb-monitor";
+const CACHE_VERSION = "zapp-store-v37-20260723-qhrb-monitor";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const STATIC_ASSETS = [
   "./",
