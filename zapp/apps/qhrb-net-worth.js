416b5d2f2e4bb2b8b2e241821361ac1ecf704b11
ziyin <ziyin@example.com>
2026-07-24 21:40:13 +0800

    chore(zapp): update QHRB net worth data

--- zapp/apps/qhrb-net-worth.js
+++ zapp/apps/qhrb-net-worth.js
@@ -1,4 +1,4 @@
-const DATA_URL = "qhrb-san-ci-can-sai-data.json?v=20260723";
+const DATA_URL = "qhrb-san-ci-can-sai-data.json?v=20260724";
 const UPDATE_LOG_URL = "qhrb-net-worth-update-log.json";
 
 const els = {
