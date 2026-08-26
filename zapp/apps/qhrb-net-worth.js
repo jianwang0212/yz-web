642422e1ca5886cc010d21f6e5e4484ec53c2589
ziyin <ziyin@example.com>
2026-08-26 21:47:21 +0800

    Update QHRB net worth for 2026-08-26

--- zapp/apps/qhrb-net-worth.js
+++ zapp/apps/qhrb-net-worth.js
@@ -1,4 +1,4 @@
-const DATA_URL = "qhrb-san-ci-can-sai-data.json?v=20260825";
+const DATA_URL = "qhrb-san-ci-can-sai-data.json?v=20260826";
 const UPDATE_LOG_URL = "qhrb-net-worth-update-log.json";
 
 const els = {
--- zapp/sw.js
+++ zapp/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "zapp-store-v40-20260825-qhrb-monitor";
+const CACHE_VERSION = "zapp-store-v41-20260826-qhrb-monitor";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const STATIC_ASSETS = [
   "./",
